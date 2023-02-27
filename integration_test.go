4e42552fa8db1deca3aee02370b20b2b034e80d5
Abhinav Gupta <mail@abhinavg.net>
2023-02-27 07:31:33 -0800

    test: Switch to YAML testdata
    Related:
    - abhinav/goldmark-hashtag#20
    - abhinav/goldmark-mermaid#14

--- integration_test.go
+++ integration_test.go
@@ -1,21 +1,43 @@
 package toc_test
 
 import (
+	"bytes"
+	"os"
 	"testing"
 
+	"github.com/stretchr/testify/require"
 	"github.com/yuin/goldmark"
 	"github.com/yuin/goldmark/parser"
-	goldtestutil "github.com/yuin/goldmark/testutil"
 	"go.abhg.dev/goldmark/toc"
+	"gopkg.in/yaml.v3"
 )
 
 func TestIntegration(t *testing.T) {
 	t.Parallel()
+
+	testsdata, err := os.ReadFile("testdata/tests.yaml")
+	require.NoError(t, err)
+
+	var tests []struct {
+		Desc string `yaml:"desc"`
+		Give string `yaml:"give"`
+		Want string `yaml:"want"`
+	}
+	require.NoError(t, yaml.Unmarshal(testsdata, &tests))
 
 	md := goldmark.New(
 		goldmark.WithExtensions(&toc.Extender{}),
 		goldmark.WithParserOptions(parser.WithAutoHeadingID()),
 	)
 
-	goldtestutil.DoTestCaseFile(md, "testdata/tests.txt", t)
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.Desc, func(t *testing.T) {
+			t.Parallel()
+
+			var buf bytes.Buffer
+			require.NoError(t, md.Convert([]byte(tt.Give), &buf))
+			require.Equal(t, tt.Want, buf.String())
+		})
+	}
 }
